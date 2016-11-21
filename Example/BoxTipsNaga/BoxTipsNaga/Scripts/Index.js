c22cb0dfe8795161997dbd05d6fec2e222544230
Rafael Kenji Nagai <rafakenji23@gmail.com>
2016-11-21 16:47:13 -0200

    ajuste no script modelo

--- Example/BoxTipsNaga/BoxTipsNaga/Scripts/Index.js
+++ Example/BoxTipsNaga/BoxTipsNaga/Scripts/Index.js
@@ -1,7 +1,7 @@
 ﻿$(function () {
     BoxTip.AddTip(".box1", "Este elemento é quadrado e vermelho.");
     BoxTip.AddTip(".box2", "Este botão pertence ao grupo dos felinos");
-    BoxTip.AddTip(".box3", "vamos testar com varios textos complexos para ver se quebra o layout do meu Tip", "Aviso");
+    BoxTip.AddTip(".box3", "E este aqui é azul, clique sobre ele para concluir.", "Aviso");
     BoxTip.AddTip(".box4", "Mensagem 1", "Aviso");
     BoxTip.AddTip(".box5", "este teste é em um botao tipo Button, em tags de input nao funciona o tips pois ele nao deixa eu criar dentro o tips", "Aviso");
     BoxTip.AddTip(".box6", "Preciso aprimorar este tips para que quando colocar uma classe igual ele coloque o tip de acordo com ele na tela", "Aviso");
